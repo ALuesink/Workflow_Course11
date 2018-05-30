30f95c49cd8b59ff1746a87fd693bd870aa737b0
ALuesink <anneluesink@live.nl>
2018-05-30 20:36:04 +0200

    Added Open_Bestand.py to the Snakefile

---
+++ Scripts/Open_Bestand.py
@@ -0,0 +1,25 @@
+def main():
+    openfile()
+
+def openfile():
+    lines = []
+    ids = []
+    with open("Data/RNA-Seq-counts.txt", "rb") as f:
+        contents = f.readlines()[2:]
+    for line in contents:
+        regel = str(line.strip(),"utf-8")
+        regel = regel.split("\t")
+        id = regel[0]
+        lines.append(regel)
+        ids.append(id)
+
+    # file = open("Data/RNA-Seq-IDs.txt", "w+")
+    # for id in ids:
+    #     file.write(id + "\n")
+    # file.close()
+
+    with open("Data/RNA-Seq-IDs.txt", "w+") as file:
+        for id in ids:
+            file.write(id + '\n')
+
+main()
--- Snakefile
+++ Snakefile
@@ -1,1 +1,7 @@
-#Start Snakefile
+rule open_file:
+	input:
+		"Data/RNA-Seq-counts.txt"
+	output:
+		"Data/RNA-Seq-IDs.txt"
+	shell:
+		"python Scripts/Open_Bestand.py"

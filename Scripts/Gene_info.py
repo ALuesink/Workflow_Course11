f03806a9a147958e5dfa7b374de2fda893bfd22e
ALuesink <anneluesink@live.nl>
2018-05-31 17:01:19 +0200

    Added Gene_info to GitHub and to Snakefile

---
+++ Scripts/Gene_info.py
@@ -0,0 +1,51 @@
+import sys
+from Bio import Entrez
+
+def main():
+    file_input = sys.argv[1]
+    file_output = sys.argv[2]
+    list_PMIDs = openfile(file_input)
+    PMID_connection(list_PMIDs, file_output)
+
+def openfile(file):
+    list_PMIDs = []
+
+    with open(file, "r") as f:
+        for line in f:
+            regel = line.split("\t")
+            lp = regel[0]
+            id = regel[1].rstrip()
+            list_PMIDs.append([lp,id])
+
+    return list_PMIDs
+
+def PMID_connection(list_PMIDs, file):
+    Entrez.email = "mail@email.com"
+
+    with open(file, "w+") as outfile:
+            for PMID in list_PMIDs:
+                id = PMID[1]
+                locus_tag = PMID[0]
+
+                handle = Entrez.efetch(db="gene", id=id, retmode="xml")
+                record = Entrez.read(handle)
+
+                genoom = record[0]['Entrezgene_gene-source']['Gene-source']['Gene-source_src-str1']
+                gen_naam = record[0]['Entrezgene_locus'][0]['Gene-commentary_products'][0]['Gene-commentary_label']
+                gen_accessie = record[0]['Entrezgene_locus'][0]['Gene-commentary_products'][0]['Gene-commentary_accession']
+                gen_start = record[0]['Entrezgene_locus'][0]['Gene-commentary_seqs'][0]['Seq-loc_int']['Seq-interval']['Seq-interval_from']
+                gen_stop = record[0]['Entrezgene_locus'][0]['Gene-commentary_seqs'][0]['Seq-loc_int']['Seq-interval']['Seq-interval_to']
+                gen_refs = record[0]['Entrezgene_locus'][0]['Gene-commentary_products'][0]['Gene-commentary_refs']
+                pubmed_refs = []
+                for ref in gen_refs:
+                    ref = ref['Pub_pmid']['PubMedId']
+                    pubmed_refs.append(ref)
+
+                outfile.write(locus_tag+"\t"+gen_naam+"\t"+gen_accessie+"\t"+genoom+"\t"+gen_start+"\t"+gen_stop+"\t"+str(pubmed_refs)+"\n")
+
+
+
+
+
+
+main()
--- Snakefile
+++ Snakefile
@@ -13,3 +13,11 @@ rule get_PMIDs:
 		"Data/Oefen_PMIDs.txt"
 	shell:
 		"python Scripts/Get_PMIDs.py {input} {output}"
+
+rule gene_info:
+	input:
+		"Data/Oefen_PMIDs.txt"
+	output:
+		"Data/Oefen_Gen_info.txt"
+	shell:
+		"python Scripts/Gene_info.py {input} {output}"

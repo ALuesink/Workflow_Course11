fecdabd17e093c6debf7525fe1d7a0961c2836ec
ALuesink <anneluesink@live.nl>
2018-06-05 16:24:41 +0200

    Removed extra echo's

--- Scripts/Uniprot_info.sh
+++ Scripts/Uniprot_info.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 while IFS=$'\t' read -r -a myArray
 do
-  # echo $myArray
   id=$"${myArray[1]}"
   lp=$"${myArray[0]}"
-  # echo $id
   data=$(curl -s "https://www.uniprot.org/uniprot/${id}.txt")
   GO=$(egrep -o "[FCP]:[0-9a-zA-Z ]*;" <<< "$data" | tr '\n' ' ')
   echo -e "${lp} \t ${GO}"
